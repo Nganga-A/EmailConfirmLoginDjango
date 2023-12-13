149845248b888d5935d1a2e533e0526ed961801b
Abed Nganga <ngangaabed@gmail.com>
2023-12-13 17:40:49 +0300

    Updated templates and modified settings

--- EmailConfirmLoginDjango/EmailConfirmLoginDjango/settings.py
+++ EmailConfirmLoginDjango/EmailConfirmLoginDjango/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     ##crispy_forms
     'crispy_forms',
+    'crispy_bootstrap4',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' 
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
